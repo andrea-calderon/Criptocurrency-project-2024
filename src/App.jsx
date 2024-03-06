393d64ecf419c1bcd46f32c69dd3f46a5aa21bac
Andrea Calderon <alvanesc@gmail.com>
2024-03-05 19:28:39 -0600

    Criptocurrency project initialized

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        
+      <h1>Welcome to my criptocurrency project</h1>
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App;
